578ba4d879cba45377fed63d4d382f87cc234797
EunseokEom <me@eseom.org>
2016-01-11 00:48:19 +0900

    add default config file, remove bootstrap code

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'development' => [
+        'name' => 'Hasty-Skeleton',
+        'dbconn' => [
+            'driver' => 'pdo_sqlite',
+            'path' => '/tmp/db.sqlite3'
+        ],
+        'debug' => true,
+    ],
+];
--- public/index.php
+++ public/index.php
@@ -1,43 +1,5 @@
 <?php
 
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+require_once('../vendor/autoload.php');
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-require_once(ROOT . 'vendor/autoload.php');
-
-function dump($obj)
-{
-    if (php_sapi_name() == 'cli') {
-        print_r($obj);
-        echo "\n";
-    } else {
-        echo '<pre>';
-        print_r($obj);
-        echo '</pre>';
-    }
-}
-
-
-class JsonifiedObject
-{
-    public $value;
-
-    public function __construct($value)
-    {
-        $this->value = json_encode($value, JSON_PRETTY_PRINT);
-    }
-
-    public function getValue() {
-        return $this->value;
-    }
-}
-
-function jsonify($obj)
-{
-    return new JsonifiedObject($obj);
-}
-
-\Hasty\Entrance::bootstrap('../app');
+\Hasty\App::run('../app');
